e1fc310dc20e437585842a869af045da0e5d43e8
OmniZlatoon <obaseviv@gmail.com>
2026-06-28 05:29:08 +0100

    Push notification set up

--- ignition-mobile/lib/main.dart
+++ ignition-mobile/lib/main.dart
@@ -1,9 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'app.dart';
+import 'core/push_notification_service.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
+  
+  // Initialize Push Notifications
+  await PushNotificationService().init();
+  
   runApp(const IgnitionPayApp());
 }
